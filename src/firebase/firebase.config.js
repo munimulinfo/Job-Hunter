6b9914d6f3e08b3ae2b32e79a73b0703f8c93d15
munimulinfo <munimul.info@gmail.com>
2023-07-03 19:19:56 +0600

    file uploded'

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQx3uSy3HSTTPLToisLq_yIMKG-xo6PVM",
+  authDomain: "practice-jobportal.firebaseapp.com",
+  projectId: "practice-jobportal",
+  storageBucket: "practice-jobportal.appspot.com",
+  messagingSenderId: "412282609830",
+  appId: "1:412282609830:web:32f012b5a51adaf9b8d467"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
